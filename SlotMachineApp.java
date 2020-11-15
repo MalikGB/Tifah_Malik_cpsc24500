e52bd7d7109cc0cb3e917ee55e53beed0d703f7c
malik tifah <MalikTifah378@gmail.com>
2020-11-15 11:33:40 -0600

    Submission of the vegas baby part 1 assignment

---
+++ SlotMachineApp.java
@@ -0,0 +1,15 @@
+/**
+ * This class is used to run the program
+ * @author Malik Tifah
+ *
+ */
+public class SlotMachineApp {
+	/**
+	 * Contains slotmachineframe object that makes the frame visible
+	 * @param args argument for the main function
+	 */
+	public static void main(String[] args) {
+		SlotMachineFrame sl = new SlotMachineFrame(); 
+		sl.setVisible(true);
+	}
+}
